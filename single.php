af2c97da1e942f2d39edc634611a0becbc35dd8e
Stavros <stavrossavva90@gmail.com>
2026-03-10 15:18:08 +0000

    add related articles to blog

--- single.php
+++ single.php
@@ -15,6 +15,58 @@
 
         <div class="the-content">
           <?php the_content(); ?>
+
+          <!-- Related Articles -->
+          <div class="related-articles">
+            <div class="related-articles-title">Related Articles</div>
+            <div class="articles-grid">
+              <?php
+              $categories = get_the_category();
+              $category_ids = array();
+              
+              foreach($categories as $category) {
+                $category_ids[] = $category->term_id;
+              }
+              
+              if(!empty($category_ids)) {
+                $args = array(
+                  'category__in' => $category_ids,
+                  'posts_per_page' => 3,
+                  'post__not_in' => array(get_the_ID()),
+                  'orderby' => 'date',
+                  'order' => 'DESC'
+                );
+                
+                $related_posts = new WP_Query($args);
+                
+                if($related_posts->have_posts()) {
+                  while($related_posts->have_posts()) {
+                    $related_posts->the_post();
+                    ?>
+                    <div class="related-article-card">
+                      <a href="<?php the_permalink(); ?>" class="article-image-link">
+                        <?php if(has_post_thumbnail()) : ?>
+                          <?php the_post_thumbnail('medium'); ?>
+                        <?php endif; ?>
+                      </a>
+                      <h3 class="article-title">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                      </h3>
+                      <p class="article-excerpt">
+                        <?php 
+                          $excerpt = wp_trim_words(get_the_excerpt(), 10, '...');
+                          echo $excerpt;
+                        ?>
+                      </p>
+                    </div>
+                    <?php
+                  }
+                  wp_reset_postdata();
+                }
+              }
+              ?>
+            </div>
+          </div>
         </div>
       </article>
 
